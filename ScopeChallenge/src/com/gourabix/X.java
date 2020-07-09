6d165925cb5c052e384e9645fd326459c89c3c1e
Gourab Sarkar <GourabIX@users.noreply.github.com>
2020-07-09 19:36:01 +0530

    Added source code for the Scope challenge problem

---
+++ ScopeChallenge/src/com/gourabix/X.java
@@ -0,0 +1,17 @@
+package com.gourabix;
+
+import java.util.Scanner;
+
+public class X {
+    private int x = -1;
+
+    public X(Scanner x) {
+        this.x = x.nextInt();
+    }
+
+    public void x() {
+        for (int x = 1; x <= 12; x++) {
+            System.out.println(this.x + " times " + x + " = " + this.x * x);
+        }
+    }
+}
