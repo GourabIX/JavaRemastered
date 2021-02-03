f1067851673312d016a8905d7897bcae854f7275
Gourab Sarkar <GourabIX@users.noreply.github.com>
2021-02-04 04:36:48 +0530

    Added Lambda expressions for multiple params and in foreach loop

---
+++ LambdaExpressions/src/com/gourabix/java8/Addable.java
@@ -0,0 +1,8 @@
+package com.gourabix.java8;
+
+@FunctionalInterface
+public interface Addable {
+	
+	int add(int num1, int num2);
+
+}
--- LambdaExpressions/src/com/gourabix/java8/LambdaExpressionsExampleMain.java
+++ LambdaExpressions/src/com/gourabix/java8/LambdaExpressionsExampleMain.java
@@ -1,5 +1,8 @@
 package com.gourabix.java8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LambdaExpressionsExampleMain {
 
 	public static void main(String[] args) {
@@ -8,11 +11,22 @@ public class LambdaExpressionsExampleMain {
 		};
 
 		drawable.draw();
-		
+
 		Sayable sayable = (name) -> {
 			System.out.println("Currently saying " + name);
 		};
 		sayable.say("Sush, I Love You.");
+
+		Addable addable = (n1, n2) -> (n1 + n2);
+		System.out.println("Sum: " + addable.add(4, 5));
+
+		List<String> nameList = new ArrayList<String>();
+		nameList.add("Badal Sarkar");
+		nameList.add("Rita Sarkar");
+		nameList.add("Gourab Sarkar");
+		nameList.add("Sush Sarkar");
+
+		nameList.forEach(name -> System.out.println(name));
 	}
 
 }
